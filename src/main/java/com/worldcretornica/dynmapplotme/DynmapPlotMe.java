c641baabb6d5fda2e41d7f5af5f4d8f83af48704
Matt <mattbdev@outlook.com>
2015-01-29 14:15:14 -0500

    Refreactoring and minor optimizations.

--- src/main/java/com/worldcretornica/dynmapplotme/DynmapPlotMe.java
+++ src/main/java/com/worldcretornica/dynmapplotme/DynmapPlotMe.java
@@ -2,19 +2,16 @@ package com.worldcretornica.dynmapplotme;
 
 import com.worldcretornica.plotme_core.Plot;
 import com.worldcretornica.plotme_core.PlotMeCoreManager;
-import com.worldcretornica.plotme_core.PlotMe_Core;
-import com.worldcretornica.plotme_core.api.ILocation;
-import com.worldcretornica.plotme_core.api.IWorld;
-import com.worldcretornica.plotme_core.bukkit.PlotMe_CorePlugin;
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.Listener;
-import org.bukkit.event.server.PluginEnableEvent;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginManager;
-import org.bukkit.plugin.java.JavaPlugin;
+import com.worldcretornica.plotme_core.bukkit.*;
+import com.worldcretornica.plotme_core.bukkit.api.*;
+import com.worldcretornica.plotme_core.bukkit.event.*;
+import org.bukkit.*;
+import org.bukkit.configuration.*;
+import org.bukkit.configuration.file.*;
+import org.bukkit.event.*;
+import org.bukkit.event.server.*;
+import org.bukkit.plugin.*;
+import org.bukkit.plugin.java.*;
 import org.dynmap.DynmapAPI;
 import org.dynmap.markers.AreaMarker;
 import org.dynmap.markers.MarkerAPI;
@@ -25,6 +22,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class DynmapPlotMe extends JavaPlugin {
 
@@ -32,26 +30,34 @@ public class DynmapPlotMe extends JavaPlugin {
                                                  " Owner <span style=\"font-weight:bold;\">%plotowners%</span>%plothelpers%";
     private static MarkerSet set;
     public PlotMe_CorePlugin plotme;
-    public MarkerAPI markerAPI;
+    public MarkerAPI markerapi;
     public Plugin dynmap;
     public DynmapAPI api;
     private long updperiod;
-    private String infoWindow;
+    // private boolean use3d;
+    private String infowindow;
     private AreaStyle defstyle;
     private Map<String, AreaStyle> cusstyle;
     private Map<String, AreaStyle> cuswildstyle;
-    private Map<String, AreaStyle> ownerStyle;
+    private Map<String, AreaStyle> ownerstyle;
     private Set<String> visible;
     private Set<String> hidden;
     private boolean stop;
     private Map<String, AreaMarker> resareas = new HashMap<>();
-    private PlotMe_Core plotmeAPI;
+
+    public void info(String msg) {
+        getLogger().info("[DynmapPlotMe] " + msg);
+    }
+
+    public void severe(String msg) {
+        getLogger().severe("[DynmapPlotMe] " + msg);
+    }
 
     private String formatInfoWindow(Plot plot) {
-        String v = "<div class=\"plotinfo\">" + infoWindow + "</div>";
+        String v = "<div class=\"plotinfo\">" + infowindow + "</div>";
         v = v.replace("%plotid%", plot.getId());
         v = v.replace("%plotowners%", plot.getOwner());
-        if (plot.getAllowed().isEmpty()) {
+        if (plot.getAllowed().equalsIgnoreCase("")) {
             v = v.replace("%plothelpers%", "");
         } else {
             v = v.replace("%plothelpers%", "<br />Helpers <span style=\"font-weight:bold;\">" + plot.getAllowed() + "</span></div>");
@@ -90,9 +96,9 @@ public class DynmapPlotMe extends JavaPlugin {
             }
         }
         if (as == null) {    /* Check for owner style matches */
-            if (!ownerStyle.isEmpty()) {
+            if (!ownerstyle.isEmpty()) {
                 String owner = plot.getOwner();
-                as = ownerStyle.get(owner.toLowerCase());
+                as = ownerstyle.get(owner.toLowerCase());
             }
         }
         if (as == null) {
@@ -119,19 +125,25 @@ public class DynmapPlotMe extends JavaPlugin {
      * @param plot
      * @param newmap
      */
-    private void handlePlot(IWorld world, Plot plot, Map<String, AreaMarker> newmap) {
+    private void handlePlot(World world, Plot plot, Map<String, AreaMarker> newmap) {
         String name = plot.getId();
 
         /* Handle areas */
         if (isVisible(name, world.getName())) {
 
-            ILocation bottom = PlotMeCoreManager.getPlotBottomLoc(world, name);
-            ILocation top = PlotMeCoreManager.getPlotTopLoc(world, name);
+            PlotMeCoreManager manager = PlotMeCoreManager.getInstance();
+            Location bottom = ((BukkitLocation) manager.getPlotBottomLoc(new BukkitWorld(world), name)).getLocation();
+            Location top = ((BukkitLocation) manager.getPlotTopLoc(new BukkitWorld(world), name)).getLocation();
+
+            int roadheight = plotme.getAPI().getGenManager(world.getName()).getRoadHeight(world.getName());
+
+            bottom.setY(roadheight);
+            top.setY(roadheight);
 
             /* Make outline */
             double[] x = new double[4];
-            x[0] = bottom.getX() - 2.0;
             double[] z = new double[4];
+            x[0] = bottom.getX() - 2.0;
             z[0] = bottom.getZ() - 2.0;
             x[1] = bottom.getX() - 2.0;
             z[1] = top.getZ() + 2.0;
@@ -151,7 +163,9 @@ public class DynmapPlotMe extends JavaPlugin {
                 m.setCornerLocations(x, z); /* Replace corner locations */
                 m.setLabel(name);   /* Update label */
             }
-
+            //if(use3d) { /* If 3D? */
+            m.setRangeY(top.getY(), bottom.getY());
+            //}
             /* Set line and fill properties */
             addStyle(name, world.getName(), m, plot);
 
@@ -170,12 +184,13 @@ public class DynmapPlotMe extends JavaPlugin {
         Map<String, AreaMarker> newmap = new HashMap<>(); /* Build new map */
 
         /* Loop through worlds */
-        for (IWorld world : plotmeAPI.getServerBridge().getWorlds()) {
-            if (plotmeAPI.getPlotMeCoreManager().isPlotWorld(world)) {
-                HashMap<String, Plot> plots = plotmeAPI.getSqlManager().getPlots(world.getName().toLowerCase());
+        for (World w : getServer().getWorlds()) {
+            BukkitWorld world = new BukkitWorld(w);
+            if (PlotMeCoreManager.getInstance().isPlotWorld(world)) {
+                ConcurrentHashMap<String, Plot> plots = PlotMeCoreManager.getInstance().getMap(world).getLoadedPlots();
 
                 for (Plot plot : plots.values()) {
-                    handlePlot(world, plot, newmap);
+                    handlePlot(w, plot, newmap);
                 }
             }
         }
@@ -193,25 +208,26 @@ public class DynmapPlotMe extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        PluginManager pluginManager = getServer().getPluginManager();
+        info("initializing");
+        PluginManager pm = getServer().getPluginManager();
         /* Get dynmap */
-        dynmap = pluginManager.getPlugin("dynmap");
+        dynmap = pm.getPlugin("dynmap");
         if (dynmap == null) {
-            getLogger().severe("Cannot find dynmap!");
+            severe("Cannot find dynmap!");
             return;
         }
         api = (DynmapAPI) dynmap; /* Get API */
         /* Get WorldGuard */
-        Plugin plotMe = pluginManager.getPlugin("PlotMe");
-        if (plotMe == null) {
-            getLogger().severe("Cannot find PlotMe!");
+        Plugin p = pm.getPlugin("PlotMe-Core");
+        if (p == null) {
+            severe("Cannot find PlotMe-Core!");
             return;
         }
+        plotme = (PlotMe_CorePlugin) p;
 
-        plotme = (PlotMe_CorePlugin) plotMe;
-        plotmeAPI = plotme.getAPI();
         getServer().getPluginManager().registerEvents(new OurServerListener(), this);
         /* If both enabled, activate */
+
         if (dynmap.isEnabled() && plotme.isEnabled()) {
             activate();
         }
@@ -220,9 +236,9 @@ public class DynmapPlotMe extends JavaPlugin {
     public void activate() {
 
         /* Now, get markers API */
-        markerAPI = api.getMarkerAPI();
-        if (markerAPI == null) {
-            getLogger().severe("Error loading dynmap marker API!");
+        markerapi = api.getMarkerAPI();
+        if (markerapi == null) {
+            severe("Error loading dynmap marker API!");
             return;
         }
         /* Load configuration */
@@ -231,14 +247,14 @@ public class DynmapPlotMe extends JavaPlugin {
         saveConfig();  /* Save updates, if needed */
 
         /* Now, add marker set for mobs (make it transient) */
-        set = markerAPI.getMarkerSet("plotme.markerset");
+        set = markerapi.getMarkerSet("plotme.markerset");
         if (set == null) {
-            set = markerAPI.createMarkerSet("plotme.markerset", cfg.getString("layer.name", "PlotMe"), null, false);
+            set = markerapi.createMarkerSet("plotme.markerset", cfg.getString("layer.name", "PlotMe"), null, false);
         } else {
             set.setMarkerSetLabel(cfg.getString("layer.name", "PlotMe"));
         }
         if (set == null) {
-            getLogger().severe("Error creating marker set");
+            severe("Error creating marker set");
             return;
         }
         int minzoom = cfg.getInt("layer.minzoom", 0);
@@ -247,12 +263,13 @@ public class DynmapPlotMe extends JavaPlugin {
         }
         set.setLayerPriority(cfg.getInt("layer.layerprio", 10));
         set.setHideByDefault(cfg.getBoolean("layer.hidebydefault", false));
-        infoWindow = cfg.getString("infowindow", DEF_INFOWINDOW);
+        //use3d = cfg.getBoolean("use3dregions", false);
+        infowindow = cfg.getString("infowindow", DEF_INFOWINDOW);
 
         /* Get style information */
         defstyle = new AreaStyle(cfg, "plotstyle");
         cusstyle = new HashMap<>();
-        ownerStyle = new HashMap<>();
+        ownerstyle = new HashMap<>();
         cuswildstyle = new HashMap<>();
         ConfigurationSection sect = cfg.getConfigurationSection("custstyle");
         if (sect != null) {
@@ -271,7 +288,7 @@ public class DynmapPlotMe extends JavaPlugin {
             Set<String> ids = sect.getKeys(false);
 
             for (String id : ids) {
-                ownerStyle.put(id.toLowerCase(), new AreaStyle(cfg, "ownerstyle." + id, defstyle));
+                ownerstyle.put(id.toLowerCase(), new AreaStyle(cfg, "ownerstyle." + id, defstyle));
             }
         }
         List<String> vis = cfg.getStringList("visibleplots");
@@ -284,7 +301,7 @@ public class DynmapPlotMe extends JavaPlugin {
         }
 
         /* Set up update job - based on periond */
-        int per = cfg.getInt("update.period", 60);
+        int per = cfg.getInt("update.period", 300);
         if (per < 15) {
             per = 15;
         }
@@ -292,6 +309,15 @@ public class DynmapPlotMe extends JavaPlugin {
         stop = false;
 
         getServer().getScheduler().scheduleSyncDelayedTask(this, new PlotMeUpdate(), 40);   /* First time is 2 seconds */
+    }
+
+    public void Disable() {
+        if (set != null) {
+            set.deleteMarkerSet();
+            set = null;
+        }
+        resareas.clear();
+        stop = true;
     }
 
     private static class AreaStyle {
@@ -303,7 +329,7 @@ public class DynmapPlotMe extends JavaPlugin {
         double fillopacity;
         String label;
 
-        AreaStyle(ConfigurationSection cfg, String path, AreaStyle def) {
+        AreaStyle(FileConfiguration cfg, String path, AreaStyle def) {
             strokecolor = cfg.getString(path + ".strokeColor", def.strokecolor);
             strokeopacity = cfg.getDouble(path + ".strokeOpacity", def.strokeopacity);
             strokeweight = cfg.getInt(path + ".strokeWeight", def.strokeweight);
@@ -312,7 +338,7 @@ public class DynmapPlotMe extends JavaPlugin {
             label = cfg.getString(path + ".label", null);
         }
 
-        AreaStyle(ConfigurationSection cfg, String path) {
+        AreaStyle(FileConfiguration cfg, String path) {
             strokecolor = cfg.getString(path + ".strokeColor", "#FF0000");
             strokeopacity = cfg.getDouble(path + ".strokeOpacity", 0.8);
             strokeweight = cfg.getInt(path + ".strokeWeight", 8);
@@ -333,15 +359,28 @@ public class DynmapPlotMe extends JavaPlugin {
 
     private class OurServerListener implements Listener {
 
-        @EventHandler(priority = EventPriority.MONITOR)
+        @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
         public void onPluginEnable(PluginEnableEvent event) {
-            Plugin plugin = event.getPlugin();
-            String name = plugin.getDescription().getName();
-            if ("dynmap".equals(name)) {
+            Plugin p = event.getPlugin();
+            String name = p.getDescription().getName();
+            if (name.equals("dynmap")) {
                 if (dynmap.isEnabled() && plotme.isEnabled()) {
                     activate();
                 }
             }
         }
+
+        @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+        public void onPlotEvent(PlotEvent event) {
+            Plot plot = event.getPlot();
+            World w = event.getWorld();
+
+            if (plot != null && w != null) {
+                Map<String, AreaMarker> newmap = new HashMap<>();
+
+                handlePlot(w, plot, newmap);
+            }
+        }
     }
+
 }
